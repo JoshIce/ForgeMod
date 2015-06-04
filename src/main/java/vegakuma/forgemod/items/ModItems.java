9233635d5fc5f051a9d410e87b9fb6adcf6bb434
Josh Ice <josh.j.ice@gmail.com>
2015-06-03 22:41:32 -0700

    added moditems class

---
+++ src/main/java/vegakuma/forgemod/items/ModItems.java
@@ -0,0 +1,12 @@
+package vegakuma.forgemod.items;
+
+import net.minecraft.item.Item;
+
+public final class ModItems {
+
+    public static Item vegakumaItem;
+
+    public static void init(){
+        vegakumaItem = new ItemVegakuma();
+    }
+}
